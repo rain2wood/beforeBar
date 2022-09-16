6e653da6b423a699a82f63f43c8e6b972474edad
Wiley Lau <henloboii@protonmail.ch>
2022-09-16 21:01:26 +0800

    Initial code drop.

---
+++ main.ino
@@ -0,0 +1,35 @@
+#define fPIR 9 // front PIR data pin
+#define rPIR 12 // rear PIR data pin
+#define percentage 60 // percentage of allowed personnel
+#define maxcap 120 // max capacity of people in the venue
+ 
+void setup() {
+  pinMode(fPIR, OUTPUT);
+  pinMode(rPIR, OUTPUT);
+  Serial.begin(9600);
+}
+
+int pnum = 0;
+
+void loop() {
+  /* if (pnum >= (maxcap * (perfentage / 100)) ) {
+    // add your own hooks to warn user here
+  } */
+  while (digitalRead(fPIR) == HIGH) {
+    for (int i = 0; i <= 10 && in != 1, i++) {
+      if (digitalRead(rPIR) == HIGH) {
+        pnum = pnum + 1;
+      }
+      delay(100);
+    }
+  }
+  while (digitalRead(rPIR) == HIGH) {
+    for (int i = 0; i <= 10 && in != 1, i++) {
+      if (digitalRead(fPIR) == HIGH) {
+        pnum = pnum - 1;
+      }
+      delay(100);
+    }
+  }
+  delay(1000);
+}
